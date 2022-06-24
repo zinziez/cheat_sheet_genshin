91880bc88be038edf1cd9bb3055bcced9ba4c1d2
Xanthus <Xanthus58@protonmail.com>
2022-06-24 15:16:02 +0930

    feat: add material demo
    Added material demo for @zinziez to fill in

---
+++ src/char_materials.rs
@@ -0,0 +1,21 @@
+use colour::*;
+
+pub fn venti() {
+    println!("Ventis mat reqirements");
+    println!("-------------------");
+    green_ln!("5x Gold Bars");
+    green_ln!("5x Iron Bars");
+    green_ln!("5x Wood");
+    green_ln!("5x Stone");
+    green_ln!("5x Leather");
+}
+
+pub fn sayu() {
+    println!("Sayus mat reqirements");
+    println!("-------------------");
+    green_ln!("5x Gold Bars");
+    green_ln!("5x Iron Bars");
+    green_ln!("5x Wood");
+    green_ln!("5x Stone");
+    green_ln!("5x Leather");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 //Importing required libraries
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+mod char_materials;
+use char_materials::*;
+
+//A function that clears the screen
+fn cls(){
+    print!("{esc}c", esc = 27 as char); 
+}
 
 //A function that reads each line from character_list.txt and returns a vector of strings
 fn read_file() -> Vec<String> {
@@ -19,17 +26,41 @@ fn read_user_input() -> String {
     std::io::stdin()
         .read_line(&mut input)
         .unwrap();
-    let input = input.trim().to_string();
+    let input = input.trim().to_string().to_lowercase();
+    return input;    
+}
+
+//function that reads user input before exiting the program
+fn read_user_input_exit() -> String {
+    println!("\nPress enter to exit");
+    let mut input = String::new();
+    std::io::stdin()
+        .read_line(&mut input)
+        .unwrap();
+    let input = input.trim().to_string().to_lowercase();
+    if input == "exit" {
+        std::process::exit(0);
+    }
     return input;    
 }
 
 //The main function where the core of the program is executed
 fn main() {
+    cls();
+    println!("Welcome to the Genshin Cheat Sheet!");
+    println!("Please type the name of the character you want to learn about.");
+    println!("-------------------------------");
     let character = read_user_input();
     let charlist = read_file();
     for x in charlist {
         if x == character {
-            println!("{} is a character", character);
+            cls();
+            if character == "venti" {
+                venti();
+            } else if character == "sayu" {
+                sayu();
+            }
+            read_user_input_exit();
         }
     }
 }
