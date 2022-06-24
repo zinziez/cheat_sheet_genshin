0680913ebc1e11ba906a21c13c369df17054990d
Xanthus <Xanthus58@protonmail.com>
2022-06-24 13:35:15 +0930

    Fix: Code cleanup
    Cleaned up some of the code and added a comment

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,36 @@
 //Importing required libraries
-use std::fs::{File, ReadDir};
+use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::io;
 
 //Defining a function to read the characters from the file
 fn read_char_file() {
     let filename = "src/character_list.txt";
     // Open the file in read-only mode (ignoring errors).
-    let file = File::open(filename).unwrap();
+    let file = File::open(filename)
+        .unwrap();
     let reader = BufReader::new(file);
     // Read the file line by line using the lines() iterator from std::io::BufRead.
-    for (index, line) in reader.lines().enumerate() {
-        let line = line.unwrap(); // Ignore errors.
+    for (index, line) in reader
+        .lines()
+        .enumerate() {
+        let line = line
+            .unwrap(); // Ignore errors.
         // Show the line and its number.
-        println!("{}. {}", index + 1, line);
+        println!("{}", line);
     }
 }
 
+//Defining a function that can read user input
 fn read_user_input() -> String {
     let mut input = String::new();
-    std::io::stdin().read_line(&mut input).unwrap();
+    std::io::stdin()
+        .read_line(&mut input)
+        .unwrap();
     return input;    
 }
 
 //The main function where the core of the program is executed
 fn main() {
-    read_user_input();
+    let character = read_user_input();
     read_char_file();
 }
